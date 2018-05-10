98288c809f356744ee7401008e658c263c046642
Tim Dumol <tim@timdumol.com>
2018-05-10 17:52:47 +0800

    Bump version: 1.1.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kalibrr/sliding_rate_limiter',
-    version='1.1.0',
+    version='2.0.0',
     zip_safe=False,
 )
